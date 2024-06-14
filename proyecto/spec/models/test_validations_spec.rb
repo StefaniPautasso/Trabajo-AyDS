9541d8fe8f6b9766ad7cb35a145548812ec36118
GiselPautasso <giselpautasso@gmail.com>
2024-06-13 22:21:35 -0300

    modifique spec_helper y test_validations

--- proyecto/spec/models/test_validations_spec.rb
+++ proyecto/spec/models/test_validations_spec.rb
@@ -1,21 +1,25 @@
-# spec/models/test_spec.rb
-
 require 'spec_helper'
 require './models/test'
 
 RSpec.describe Test do
   describe 'validations' do
     it 'is invalid without a title' do
-      test = Test.new(title: nil, section_id: 1)
+      test = Test.new(title: nil, section: Section.new)
       expect(test).to_not be_valid
       expect(test.errors[:title]).to include("can't be blank")
     end
 
-    it 'is valid with a title' do
-      test = Test.new(title: 'TituloTest', section_id: 1)
+    it 'is invalid without a section' do
+      test = Test.new(title: 'TituloTest', section: nil)
+      expect(test).to_not be_valid
+      expect(test.errors[:section]).to include("can't be blank")
+    end
+
+    it 'is valid with a title and section' do
+      section = Section.create(title: 'Section 1')
+      test = Test.new(title: 'TituloTest', section: section)
       expect(test).to be_valid
     end
   end
 end
-
 
--- proyecto/spec/spec_helper.rb
+++ proyecto/spec/spec_helper.rb
@@ -1,24 +1,47 @@
 ENV['RACK_ENV'] = 'test'
 
-require_relative '../server.rb'
-require 'rspec'
 require 'rack/test'
+require './server.rb'
+require 'rspec'
+require 'active_record'
+require 'database_cleaner'
 
 RSpec.configure do |config|
+
   config.include Rack::Test::Methods
 
   def app
     App
   end
-
+  
   config.before(:suite) do
-    ActiveRecord::Base.logger = nil
     ActiveRecord::Base.establish_connection(:test)
-    load "#{App.root}/db/schema.rb"
+    ActiveRecord::Migration.maintain_test_schema!
+  end
+
+  config.before(:each) do
+    ActiveRecord::Base.connection.begin_transaction(joinable: false)
   end
 
   config.after(:each) do
-    ActiveRecord::Base.connection.close
-  end
+    ActiveRecord::Base.connection.rollback_transaction
+  end 
+  
 end
 
+=begin
+  config.before(:suite) do
+    ActiveRecord::Base.establish_connection(:test)
+    ActiveRecord::Migration.maintain_test_schema!
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+=end
