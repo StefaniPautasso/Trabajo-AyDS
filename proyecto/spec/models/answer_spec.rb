84c44efbaa78629dac2d424165b2883c1148a386
StefaniPautasso <stefanipautasso@gmail.com>
2024-06-17 15:54:04 -0300

    Actualización de test answer y progress

--- proyecto/spec/models/answer_spec.rb
+++ proyecto/spec/models/answer_spec.rb
@@ -10,24 +10,87 @@ RSpec.describe Answer, type: :model do
   let(:user) { User.create(name: 'TestUser', password: 'password123') }
   let(:section) { Section.create(title: 'Section 1') }
   let(:test) { Test.create(title: 'Test 1', section: section) }
-  let(:question) { Question.create(content: 'Example question', test: test) }
-  let(:option) { Option.create(content: 'Option content', question: question) }
+  let(:question1) { Question.create(content: 'Example question 1', test: test) }
+  let(:question2) { Question.create(content: 'Example question 2', test: test) }
+  let(:option1) { Option.create(content: 'Option content 1', question: question1, correct: true) }
+  let(:option2) { Option.create(content: 'Option content 2', question: question1, correct: false) }
+  let(:option3) { Option.create(content: 'Option content 1', question: question2, correct: true) }
+  let(:option4) { Option.create(content: 'Option content 2', question: question2, correct: false) }
 
   describe 'associations' do
     it 'belongs to a user' do
-      answer = Answer.new(user: user, question: question, option: option)
+      answer = Answer.new(user: user, question: question1, option: option1)
       expect(answer.user).to eq(user)
     end
-    
+
     it 'belongs to a question' do
-      answer = Answer.new(user: user, question: question, option: option)
-      expect(answer.question).to eq(question)
+      answer = Answer.new(user: user, question: question1, option: option1)
+      expect(answer.question).to eq(question1)
     end
-    
+
     it 'belongs to an option' do
-      answer = Answer.new(user: user, question: question, option: option)
-      expect(answer.option).to eq(option)
+      answer = Answer.new(user: user, question: question1, option: option1)
+      expect(answer.option).to eq(option1)
+    end
+  end
+
+  describe 'save_user_responses' do
+    it 'saves user responses and deletes old ones' do
+      old_answer = Answer.create(user: user, question: question1, option: option2)
+      
+      responses = [
+        { question_id: question1.id, option_id: option1.id },
+        { question_id: question2.id, option_id: option3.id }
+      ]
+      
+      Answer.save_user_responses(user.id, test, responses)
+      
+      expect(Answer.where(user_id: user.id).count).to eq(2)
+      expect(Answer.where(user_id: user.id, question_id: question1.id).first.option).to eq(option1)
+      expect(Answer.where(user_id: user.id, question_id: question2.id).first.option).to eq(option3)
+    end
+
+    it 'creates new answers if none exist' do
+      responses = [
+        { question_id: question1.id, option_id: option1.id },
+        { question_id: question2.id, option_id: option3.id }
+      ]
+      
+      Answer.save_user_responses(user.id, test, responses)
+      
+      expect(Answer.where(user_id: user.id).count).to eq(2)
+      expect(Answer.where(user_id: user.id, question_id: question1.id).first.option).to eq(option1)
+      expect(Answer.where(user_id: user.id, question_id: question2.id).first.option).to eq(option3)
+    end
+
+    it 'replaces old answers with new ones' do
+      old_answer1 = Answer.create(user: user, question: question1, option: option2)
+      old_answer2 = Answer.create(user: user, question: question2, option: option4)
+      
+      responses = [
+        { question_id: question1.id, option_id: option1.id },
+        { question_id: question2.id, option_id: option3.id }
+      ]
+      
+      Answer.save_user_responses(user.id, test, responses)
+      
+      expect(Answer.where(user_id: user.id).count).to eq(2)
+      expect(Answer.where(user_id: user.id, question_id: question1.id).first.option).to eq(option1)
+      expect(Answer.where(user_id: user.id, question_id: question2.id).first.option).to eq(option3)
+    end
+
+    it 'cleans up old answers when questions are removed' do
+      old_answer1 = Answer.create(user: user, question: question1, option: option2)
+      old_answer2 = Answer.create(user: user, question: question2, option: option4)
+      
+      responses = [
+        { question_id: question1.id, option_id: option1.id }
+      ]
+      
+      Answer.save_user_responses(user.id, test, responses)
+      
+      expect(Answer.where(user_id: user.id).count).to eq(1)
+      expect(Answer.where(user_id: user.id, question_id: question1.id).first.option).to eq(option1)
     end
   end
 end
-
--- proyecto/spec/models/progress_spec.rb
+++ proyecto/spec/models/progress_spec.rb
@@ -2,11 +2,26 @@ require 'spec_helper'
 require './models/progress'
 require './models/user'
 require './models/test'
+require './models/section'
+require './models/question'
+require './models/option'
+require './models/answer'
 
 RSpec.describe Progress, type: :model do
   let(:user) { User.create(name: 'TestUser', password: 'password123') }
   let(:section) { Section.create(title: 'Test Section') }
   let(:test) { Test.create(title: 'Test 1', section: section) }
+  let(:question1) { Question.create(content: 'Question 1', test: test) }
+  let(:question2) { Question.create(content: 'Question 2', test: test) }
+  let(:option1) { Option.create(content: 'Option 1', question: question1, correct: true) }
+  let(:option2) { Option.create(content: 'Option 2', question: question1, correct: false) }
+  let(:option3) { Option.create(content: 'Option 1', question: question2, correct: true) }
+  let(:option4) { Option.create(content: 'Option 2', question: question2, correct: false) }
+  
+  before do
+    Answer.create(user: user, question: question1, option: option1)
+    Answer.create(user: user, question: question2, option: option3)
+  end
 
   describe 'validations' do
     before do
@@ -48,4 +63,36 @@ RSpec.describe Progress, type: :model do
       expect(progress.test).to eq(test)
     end
   end
+
+  describe 'calculate_score' do
+    before do
+      @progress = Progress.create(user: user, test: test)
+    end
+
+    it 'calculates the correct score' do
+      @progress.calculate_score(test)
+      expect(@progress.score).to eq(100.0)
+    end
+
+    it 'saves the score to the progress record' do
+      @progress.calculate_score(test)
+      expect(@progress.reload.score).to eq(100.0)
+    end
+
+    it 'calculates a score of 50 for one correct answer out of two questions' do
+      Answer.where(user: user, question: question2).destroy_all
+      @progress.calculate_score(test)
+      expect(@progress.score).to eq(50.0)
+    end
+
+    it 'calculates a score of 0 for no correct answers' do
+      Answer.where(user: user, question: question1).destroy_all
+      Answer.where(user: user, question: question2).destroy_all
+      Answer.create(user: user, question: question1, option: option2)
+      Answer.create(user: user, question: question2, option: option4)
+      @progress.calculate_score(test)
+      expect(@progress.score).to eq(0.0)
+    end
+  end
 end
+
