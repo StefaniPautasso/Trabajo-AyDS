2029adcabf50df2dfbd9fd667c635e3ec6d66d13
StefaniPautasso <stefanipautasso@gmail.com>
2024-09-29 12:51:11 -0300

    modificacion de vistas

--- proyecto/models/user.rb
+++ proyecto/models/user.rb
@@ -9,5 +9,25 @@ class User < ActiveRecord::Base
   
   validates :password, presence: true, length: { minimum: 8, message: "La contraseña debe tener por lo menos 8 caracteres" }, format: { without: /\s/, message: "La contraseña no puede contener espacios en blanco" }
   
+  def mark_as_deleted
+    update(is_deleted: true)
+  end
+  
+  def active?
+    !is_deleted
+  end
+
+  scope :active, -> { where(is_deleted: false) }
+
+  def top_scores
+    progresses.group_by(&:test_id).map do |test_id, scores|
+      scores.max_by(&:score)
+    end.compact
+  end
+
+  def total_score
+    top_scores.sum(&:score)
+  end
+
 end
 
