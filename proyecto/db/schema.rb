145e8470fb2869fd2f2494b8b0ed75edbe42739e
GiselPautasso <giselpautasso@gmail.com>
2024-09-29 12:40:57 -0300

    actualizacion Gemfile

--- proyecto/db/schema.rb
+++ proyecto/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_11_014704) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_28_001134) do
   create_table "answers", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "question_id", null: false
@@ -89,6 +89,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_11_014704) do
     t.string "password", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_deleted", default: false
+    t.integer "total_score", default: 0
   end
 
   add_foreign_key "answers", "options"
